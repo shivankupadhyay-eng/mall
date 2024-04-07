aad9789c078435f2a8c3c86c90c6a1a3e0f7a938
shivankupadhyay-eng <shivankupadhyay51@gmail.com>
2024-04-07 23:32:03 +0530

    change

--- backtask/settings.py
+++ backtask/settings.py
@@ -140,10 +140,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from datetime import timedelta
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-
-    ),
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 SIMPJWT = {
